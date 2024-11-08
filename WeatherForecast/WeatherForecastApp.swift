9356c9567ece8005e137d2dd2ed2989a8345ae2c
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-08 09:52:06 +0900

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by 東郷翔士希 on 2024/11/08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
